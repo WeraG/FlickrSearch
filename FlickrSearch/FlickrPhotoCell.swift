e633c52f45ebaf66dcdf7b32492a46592f83e72b
Weronika Golota <weronikag.wgo@gmail.com>
2017-12-11 12:37:17 +0100

    Add all project files for the first time

---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by Weronika Golota on 11.12.2017.
+//  Copyright © 2017 Weronika Golota. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+}
