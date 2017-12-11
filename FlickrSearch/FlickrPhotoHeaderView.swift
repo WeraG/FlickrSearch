7955ba8098e16dc2bd568abb279502c7fdd6563e
Weronika Golota <weronikag.wgo@gmail.com>
2017-12-11 22:42:15 +0100

    Added some more functionality

---
+++ FlickrSearch/FlickrPhotoHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoHeaderView.swift
+//  FlickrSearch
+//
+//  Created by Weronika Golota on 11.12.2017.
+//  Copyright © 2017 Weronika Golota. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoHeaderView: UICollectionReusableView {
+        
+    @IBOutlet var label: UILabel!
+}
